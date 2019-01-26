0e940e569bd45b120f71677eebda7a05975d64e7
Elise Phan <aliceconnic@gmail.com>
2019-01-26 02:29:21 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,7 +34,7 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
 
-  delete "/articles/:id" do
+  delete "/recipes/:id" do
     Recipe.destroy(params[:id])
     redirect to "/recipes"
   end  
