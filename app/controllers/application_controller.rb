56da546b5732fbcfcf132cc6f867365e59126c2e
Elise Phan <aliceconnic@gmail.com>
2019-01-26 01:13:44 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,5 +21,6 @@ class ApplicationController < Sinatra::Base
   
   post '/new' do
     @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect to "/recipes/#{recipe.id}"
   end
 end
