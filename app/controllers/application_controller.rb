4d7d47ec63a6e504d6693c8624d8c0dce9aedfd9
Elise Phan <aliceconnic@gmail.com>
2019-01-26 02:43:30 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,12 +43,6 @@ class ApplicationController < Sinatra::Base
     redirect to "/recipes/#{@recipe.id}"
   end
 
-patch '/articles/:id' do #edit action
-  @article = Article.find_by_id(params[:id])
-  @article.title = params[:title]
-  @article.content = params[:content]
-  @article.save
-  redirect to "/articles/#{@article.id}"
 end
 
 
