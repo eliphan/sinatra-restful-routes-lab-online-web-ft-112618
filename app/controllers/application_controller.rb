a68176bd460fad52dfb365ce3d064c5fdd360553
Elise Phan <aliceconnic@gmail.com>
2019-01-26 01:13:22 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,10 +15,11 @@ class ApplicationController < Sinatra::Base
   end
   
   get '/new' do
-    @recipe = Recipe.new(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    
     erb :new
   end
   
   post '/new' do
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
   end
 end
